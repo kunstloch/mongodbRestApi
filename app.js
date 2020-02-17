5dcfe107f2bb89a8e9956e6f4911692072f889ce
Alexander Scherer <alexscherer@outlook.com>
2020-02-17 12:28:45 +0100

    Init yarn

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const mongoose = require('mongoose');
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const ejs = require("ejs");
+// const mongoose = require('mongoose');
 
-const app = express();
+// const app = express();
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(express.static("public"));
+// app.use(bodyParser.urlencoded({
+//   extended: true
+// }));
+// app.use(express.static("public"));
 
-//TODO
+// //TODO
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
-});
+// app.listen(3000, function () {
+//   console.log("Server started on port 3000");
+// });
 
 
-/*
+
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -162,5 +162,3 @@ app.route("/articles/:articleTitle")
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
-
-*/
